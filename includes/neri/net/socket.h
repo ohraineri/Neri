de006a38b655b008e2ca6a7637bf3e3ca54167c2
Marcelo Raineri <mraineridev@gmail.com>
2025-08-06 19:21:35 -0300

    feat: add socket.h header file for socket definitions and includes

---
+++ includes/neri/net/socket.h
@@ -0,0 +1,16 @@
+#ifndef SOCKET_H
+#define SOCKET_H
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+
+#define PORT 8000
+
+#endif
